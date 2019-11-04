ff27d0e696a1ddd164dbc86c5d10d6af0c8951d9
naftis <pyry.rouvila@gmail.com>
2019-11-04 17:07:08 +0200

    Fail?

--- components/nav.tsx
+++ components/nav.tsx
@@ -6,6 +6,8 @@ const links = [
   { href: 'https://github.com/zeit/next.js', label: 'GitHub' }
 ].map(link => ({ ...link, key: `nav-link-${link.href}-${link.label}` }));
 
+var morxo;
+
 const Nav = () => (
   <nav>
     <ul>
