8f4eb1fc5946fe59966d2deb8b1bba543dcef9ac
Pyry Rouvila <pyry.rouvila@gmail.com>
2019-11-06 21:01:32 +0200

    Animate hero svg with Vivus

--- components/header.tsx
+++ components/header.tsx
@@ -1,91 +1,99 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Nav } from './nav';
 import { Center } from './center';
 import { PersonWithLaptop } from './person-with-laptop';
+import Vivus from 'vivus';
 
-export const Header: React.FC = () => (
-  <header className="hero">
-    <Center>
-      <Nav />
+export const Header: React.FC = () => {
+  const svg = useRef<HTMLElement>();
 
-      <section className="hero__content">
-        <article className="hero__article">
-          <h1 className="title">
-            We help teams create, test and ship better software from start to
-            finish.
-          </h1>
-          <p className="description">
-            Rare on tamperelainen, Suomen kovimmista webbikehityksen
-            ammattilaisista koostuva tiimi. Tiimiimme kuuluu tällä hetkellä
-            neljä jäsentä - jokaisella takanaan konsulttiura Suomen
-            tunnetuimmissa ohjelmistotaloissa.
-          </p>
+  useEffect(() => {
+    new Vivus('person', { duration: 170, animTimingFunction: Vivus.EASE });
+  }, []);
 
-          <section className="references">
-            <h3>Meihin luottaa</h3>
+  return (
+    <header className="hero">
+      <Center>
+        <Nav />
 
-            <div className="references__images">
-              <img src="../static/references/tieto.svg" alt="Tieto Oyj" />
-              <img
-                src="../static/references/plan.svg"
-                alt="Plan International"
-              />
-            </div>
-          </section>
-        </article>
-        <aside className="hero__image">
-          <PersonWithLaptop />
-          <div className="hero__blend"></div>
-        </aside>
-      </section>
-    </Center>
+        <section className="hero__content">
+          <article className="hero__article">
+            <h1 className="title">
+              We help teams create, test and ship better software from start to
+              finish.
+            </h1>
+            <p className="description">
+              Rare on tamperelainen, Suomen kovimmista webbikehityksen
+              ammattilaisista koostuva tiimi. Tiimiimme kuuluu tällä hetkellä
+              neljä jäsentä - jokaisella takanaan konsulttiura Suomen
+              tunnetuimmissa ohjelmistotaloissa.
+            </p>
 
-    <style jsx>{`
-      .hero {
-        background: linear-gradient(95.62deg, #181113 40.28%, #150b03 99.06%);
-        color: #fff;
-        padding: 3rem 0;
-      }
+            <section className="references">
+              <h3>Meihin luottaa</h3>
 
-      .hero__content {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-      }
+              <div className="references__images">
+                <img src="../static/references/tieto.svg" alt="Tieto Oyj" />
+                <img
+                  src="../static/references/plan.svg"
+                  alt="Plan International"
+                />
+              </div>
+            </section>
+          </article>
+          <aside className="hero__image">
+            <PersonWithLaptop />
+          </aside>
+        </section>
+      </Center>
 
-      .hero__article {
-        width: 55%;
-      }
+      <style jsx>{`
+        .hero {
+          background: linear-gradient(95.62deg, #181113 40.28%, #150b03 99.06%);
+          color: #fff;
+          padding: 3rem 0;
+        }
 
-      .title {
-        margin-top: 4rem;
-        font-family: 'Nunito Sans', sans-serif;
-        font-size: 3.3rem;
-        letter-spacing: 0.11rem;
-      }
+        .hero__content {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        }
 
-      .description {
-        line-height: 2rem;
-      }
+        .hero__article {
+          width: 55%;
+        }
 
-      .references {
-        margin-top: 3rem;
-      }
+        .title {
+          margin-top: 4rem;
+          font-family: 'Nunito Sans', sans-serif;
+          font-size: 3.3rem;
+          letter-spacing: 0.11rem;
+        }
 
-      .references h3 {
-        font-size: 1rem;
-        letter-spacing: 0.07rem;
-      }
+        .description {
+          line-height: 2rem;
+        }
 
-      .references__images {
-        display: flex;
-        align-items: center;
-        margin: 0 -1rem;
-      }
+        .references {
+          margin-top: 3rem;
+        }
 
-      .references__images img {
-        padding: 0 1rem;
-      }
-    `}</style>
-  </header>
-);
+        .references h3 {
+          font-size: 1rem;
+          letter-spacing: 0.07rem;
+        }
+
+        .references__images {
+          display: flex;
+          align-items: center;
+          margin: 0 -1rem;
+        }
+
+        .references__images img {
+          padding: 0 1rem;
+        }
+      `}</style>
+    </header>
+  );
+};
