aee538b87c69a4c0b4148ea76b1bca1028628469
Riku Rouvila <riku.rouvila@gmail.com>
2019-11-26 13:19:29 +0200

    add more color to workspace

--- components/workspace-section.tsx
+++ components/workspace-section.tsx
@@ -54,19 +54,19 @@ export const WorkspaceSection: React.FC = () => (
         width: 50%;
 
         // -2px to hide #e8e502 background color from left side
-        background: #e8e5e2 url('static/workspace.svg') -2px top no-repeat;
+        background: #fee2d7 url('static/workspace.svg') -2px top no-repeat;
         background-size: auto 105%;
       }
 
       .button {
         display: inline-block;
         padding: 0.9rem 6rem;
-        border: 5px solid #e4e4e4;
+        border: 5px solid #fee2d7;
         border-radius: 5px;
         color: #2e3439;
         text-decoration: none;
         font-weight: bold;
-        background: #e8e5e2;
+        background: #fee2d7;
         box-sizing: border-box;
         text-align: center;
         margin-top: 1.5rem;
