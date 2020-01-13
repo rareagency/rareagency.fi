dbdae2cdf45a1e592813e666f2069d4d24b1cd94
Pyry Rouvila <pyry.rouvila@gmail.com>
2020-01-13 20:27:42 +0200

    Use new domain and fix missing slash

--- generate-sitemap.ts
+++ generate-sitemap.ts
@@ -2,7 +2,7 @@
 const sitemap = require('nextjs-sitemap-generator');
 
 sitemap({
-  baseUrl: 'https://rareagency.fi',
+  baseUrl: 'https://rare.fi/',
   pagesDirectory: __dirname + '/pages',
   targetDirectory: 'out/',
   nextConfigPath: __dirname + '/next.config.js',
