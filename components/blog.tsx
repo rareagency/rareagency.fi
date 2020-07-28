65b9a7f654ece214244a7715c593eba010a326d6
Hannes Aaltonen <hannes@rareagency.fi>
2020-07-28 15:18:00 +0300

    Removed animation prefixes since nextjs uses autoprefixer.

--- components/blog.tsx
+++ components/blog.tsx
@@ -34,10 +34,6 @@ export const Blog: React.FC<BlogProps> = ({ blog, index }) => {
           opacity: 0;
           text-align: left;
 
-          -webkit-animation: fadein 1.5s; /* Safari, Chrome and Opera > 12.1 */
-          -moz-animation: fadein 1.5s; /* Firefox < 16 */
-          -ms-animation: fadein 1.5s; /* Internet Explorer */
-          -o-animation: fadein 1.5s; /* Opera < 12.1 */
           animation: fadein 1.5s;
           animation-fill-mode: forwards;
         }
@@ -67,46 +63,6 @@ export const Blog: React.FC<BlogProps> = ({ blog, index }) => {
         }
 
         @keyframes fadein {
-          from {
-            opacity: 0;
-          }
-          to {
-            opacity: 1;
-          }
-        }
-
-        /* Firefox < 16 */
-        @-moz-keyframes fadein {
-          from {
-            opacity: 0;
-          }
-          to {
-            opacity: 1;
-          }
-        }
-
-        /* Safari, Chrome and Opera > 12.1 */
-        @-webkit-keyframes fadein {
-          from {
-            opacity: 0;
-          }
-          to {
-            opacity: 1;
-          }
-        }
-
-        /* Internet Explorer */
-        @-ms-keyframes fadein {
-          from {
-            opacity: 0;
-          }
-          to {
-            opacity: 1;
-          }
-        }
-
-        /* Opera < 12.1 */
-        @-o-keyframes fadein {
           from {
             opacity: 0;
           }
